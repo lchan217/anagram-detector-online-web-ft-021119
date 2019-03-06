7c95314233d692293dd60593797c8f32716ae514
Lizzie Chan <lchan217@gmail.com>
2019-03-06 16:05:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,16 @@
-require 'pry'
+# Your code goes here!
+require "pry"
 class Anagram 
-  attr_accessor :word
+  attr_accessor :word 
   def initialize(word)
     @word = word 
   end 
-  def match(array)
-    array.select {|x| x.split("").sort == word.split("").sort}
+  
+  def match(word_array)
+    word_array.select do |element|
+      if @word.split("").sort == element.split("").sort
+        @word
+      end
+    end 
   end
 end 
